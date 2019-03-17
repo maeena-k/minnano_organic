c268df6d6c1902df45a0cf1f74baef7c2c528010
Jumpei Kawashima <pei.kureni@me.com>
2019-03-18 00:47:09 +0900

    modified assets setting to apply to the current css setting

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module MinnanoOrganic
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.assets.initialize_on_precompile = false
   end
 end
