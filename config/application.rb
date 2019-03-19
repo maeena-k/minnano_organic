5c48384c797e990831a29dd7671e3ce3bbb4b5d1
Jumpei Kawashima <pei.kureni@me.com>
2019-03-20 00:05:40 +0900

    setup test suite for rspec

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module MinnanoOrganic
     # the framework and any gems in your application.
 
     config.assets.initialize_on_precompile = false
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
