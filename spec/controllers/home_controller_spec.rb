740ee34e522259b28a97363908e45e6339590950
Jumpei Kawashima <pei.kureni@me.com>
2019-04-23 10:31:54 +0900

    setup spec test for home controller

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe 'index action' do
+    before do
+      get :index
+    end
+
+    it 'responds successfully on index page' do
+      expect(response).to be_successful
+    end
+
+    it 'returns a 200 status' do
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'about action' do
+    before do
+      get :about
+    end
+
+    it 'responds successfully on index page' do
+      expect(response).to be_successful
+    end
+
+    it 'returns a 200 status' do
+      expect(response).to have_http_status 200
+    end
+  end
+end
