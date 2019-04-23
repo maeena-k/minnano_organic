156b1488ec4f1698c398d95425ce308d50eb845d
Jumpei Kawashima <pei.kureni@me.com>
2019-04-23 13:13:20 +0900

    setup spec test for locations controller with signed in user context

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe LocationsController, type: :controller do
+  let!(:user) { create(:user) }
 
   describe 'show action' do
-    before do
-      @user = FactoryBot.create(:user)
-      sign_in @user
-      get :show
+    context "as a signed_in user" do
+      before do
+        sign_in user
+        get :show
+      end
+
+      it 'responds successfully' do
+        expect(response).to be_successful
+      end
+
+      it 'returns a 200 status' do
+        expect(response).to have_http_status 200
+      end
     end
 
-    it 'responds successfully' do
-      expect(response).to be_successful
-    end
+    context "as a user without signing in" do
+      before do
+        get :show
+      end
 
-    it 'returns a 200 status' do
-      expect(response).to have_http_status 200
+      it 'responds unsuccessfully' do
+        expect(response).not_to be_successful
+      end
+
+      it 'returns a 302 status' do
+        expect(response).to have_http_status 302
+      end
     end
   end
 end
